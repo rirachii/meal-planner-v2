7f33bb78e4873399e60551a0557a9ab2c5938fd7
Michelle Weng <82119639+rirachii@users.noreply.github.com>
2025-02-16 17:23:16 -0500

    Add API endpoints configuration

---
+++ src/api/endpoints.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://lively-crostata-509cae.netlify.app';
+
+export const API_ENDPOINTS = {
+  meals: `${BASE_URL}/get-json?key=meals.json`,
+  bagging: `${BASE_URL}/get-json?key=bagging.json`,
+  instructions: `${BASE_URL}/get-json?key=instructions.json`
+};
